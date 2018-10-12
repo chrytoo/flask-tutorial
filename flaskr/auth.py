c093ca0af633ab68411dab9c299ddece74c33a93
Милош Е. Ђурђевић <milos.om.dj@gmail.com>
2018-10-12 19:49:24 +0200

    Fixed auth

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,3 +1,16 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
