afdc6383d7b21954f064abed339824035264e577
Милош Е. Ђурђевић <milos.om.dj@gmail.com>
2018-10-12 19:20:20 +0200

    Added database and a blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    # create and configure the app
+    """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
+        # a default secret that should be overridden by instance config
         SECRET_KEY='dev',
+        # store the database in the instance folder
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
@@ -16,7 +18,7 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        app.config.update(test_config)
 
     # ensure the instance folder exists
     try:
@@ -24,9 +26,26 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'Hello, Flask!'
+        return 'Hello, World!'
+
+    # register the database commands
+    from flaskr import db
+    db.init_app(app)
+
+    # apply the blueprints to the app
+
+    from flaskr import auth
+    app.register_blueprint(auth.bp)
+    """
+    app.register_blueprint(blog.bp)
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule('/', endpoint='index')
+    """
 
     return app
